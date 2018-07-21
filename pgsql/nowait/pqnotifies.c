8510317732863d8ec318740121863ca381e35c58
Globik <gru5@yandex.ru>
2018-07-21 16:04:02 -0400

    a

--- pgsql/nowait/pqnotifies.c
+++ pgsql/nowait/pqnotifies.c
@@ -6,115 +6,139 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
+#include <errno.h>
 #define green "\x1b[32m"
 #define yellow "\x1b[33m"
 #define red "\x1b[31m"
 #define rst "\x1b[0m"
 int fuck=0;
+int done=0;
 const char*listenchannel="revents";
 void mainloop(PGconn*conn);
 void exitclean(PGconn*conn);
 void handlepgread(PGconn*conn);
 void initlisten(PGconn*conn);
 int main(){
-	const char*conninfokeys[]={"dbname",NULL};
-	const char*conninfovalues[]={"postgres",NULL};
-	PGconn*conn=PQconnectStartParams(conninfokeys,conninfovalues,0);
-	ConnStatusType status=PQstatus(conn);
-	if(status==CONNECTION_BAD){
-	fprintf(stderr, red "connection database failed %s\n" rst, PQerrorMessage(conn));
-	exitclean(conn);
-	}else{printf("Connection started?\n");}
-	if(status==CONNECTION_STARTED){printf("connection started!\n");}
+const char*conninfokeys[]={"dbname",NULL};
+const char*conninfovalues[]={"postgres",NULL};
+PGconn*conn=PQconnectStartParams(conninfokeys,conninfovalues,0);
+ConnStatusType status=PQstatus(conn);
+if(status==CONNECTION_BAD){
+fprintf(stderr, red "connection database failed %s\n" rst, PQerrorMessage(conn));
+exitclean(conn);
+}else if(status==CONNECTION_STARTED){
+printf("connection started!\n");
+}else if(status==CONNECTION_MADE){
+printf(green "Connection made.\n" rst);	
+}else{printf(yellow "connecting...\n");}
 mainloop(conn);
 PQfinish(conn);
+printf(green "***bye!***\n" rst);
+}
+
+void exitclean(PGconn*conn){
+printf(yellow "exitclean(conn) occured.\n");
+PQfinish(conn);
+exit(1);
+}
+static void han_sig(int n){
+printf("n: %d\n",n);
+printf(yellow "han_sig SIGINT occured.\n" rst);
+done=1;	
+}
+void foo(int n){
+	printf(red "ON TERMINATION\n" rst);
 	}
-	void exitclean(PGconn*conn){
-		PQfinish(conn);
-		exit(1);
-		}
-		void mainloop(PGconn*conn){
-			fd_set rfds,wfds;
-			struct timeval tv;
-			int retval;
-			int sock;
-			int done=0;
-			int connected=0;
-			//int sentlisten=0;
-		//	sock=PQsocket(conn);
-			
+void mainloop(PGconn*conn){
+fd_set rfds,wfds;
+int retval;
+int sock;
+signal(SIGINT,han_sig);	
+signal(SIGHUP,foo);
+int connected=0;
+	
 int u=PQsetnonblocking(conn,1);
 printf("non blocking: %d\n",u);
 int ud=PQisnonblocking(conn);
 // 1 nonbl, 0 bl
 printf("is non blocking? :%d\n",ud);
+
 sock=PQsocket(conn);	
 if(sock<0){
 printf("postgres socket is gone\n");
 exitclean(conn);
 }
 					
-			PostgresPollingStatusType connstatus;
-			while(!done){
-				printf("*WHILE_LOOP*\n");
-				usleep(20000);
-					//FD_ZERO(&rfds);
-					//FD_ZERO(&wfds);
-					//tv.tv_sec=2;
-					//tv.tv_usec=0;
-					if(!connected){
-						connstatus=PQconnectPoll(conn);
-						switch(connstatus){
-							case PGRES_POLLING_FAILED:
-							fprintf(stderr,"pgconn failed %s\n",PQerrorMessage(conn));
-							return;
-							case PGRES_POLLING_WRITING:
-							printf("PGRES_POLLING_WRITING\n");
-						//	FD_SET(sock,&wfds);
-							break;
-							case PGRES_POLLING_READING:
-							printf("PGRES_POLLING_READING\n");
-							FD_SET(sock,&rfds);
-							break;
-							case PGRES_POLLING_OK:
-							printf("PGRES_POLLING_OK\n");
-							connected=1;
-							initlisten(conn);
-							//FD_SET(sock,&rfds);
-							break;
-							}
-						
-						}
+PostgresPollingStatusType connstatus;
+while(!done){
+printf("*WHILE_LOOP*\n");
+//usleep(20000);
+//FD_ZERO(&rfds);
+//FD_ZERO(&wfds);
+if(!connected){
+connstatus=PQconnectPoll(conn);
+switch(connstatus){
+case PGRES_POLLING_FAILED:
+fprintf(stderr,"pgconn failed %s\n",PQerrorMessage(conn));
+done=1;
+exitclean(conn);
+break;
+case PGRES_POLLING_WRITING:
+printf("PGRES_POLLING_WRITING\n");
+//	FD_SET(sock,&wfds);
+break;
+case PGRES_POLLING_READING:
+printf("PGRES_POLLING_READING\n");
+FD_SET(sock,&rfds);
+break;
+case PGRES_POLLING_OK:
+printf(green "PGRES_POLLING_OK\n" rst);
+connected=1;
+initlisten(conn);
+break;
+}
+}else{
+	if(connstatus==PGRES_POLLING_OK){printf(red "poll OK\n" rst);}else if(connstatus==PGRES_POLLING_FAILED){
+		printf(red "pgres_polling_failed\n" rst);
+		}else{printf(green "DEFALTI\n" rst);}
+	}
+
 if(connected){
 printf("CONNECTED=true\n");
+FD_ZERO(&rfds);
+FD_SET(sock,&rfds);
 if(fuck==1){printf(green "FUCK?: %d\n" rst,fuck);}
 if(FD_ISSET(sock,&rfds)){
-printf(yellow "DO HANDLE PG READ: retval: %d\n" rst,retval);
+printf(yellow "DO HANDLE PG READ: retval: %d socket: %d\n" rst,retval,sock);
+//if(retval==-1){done=1;exitclean(conn);}
 handlepgread(conn);
 }
 }
-						retval=select(sock+1,&rfds,NULL,NULL,NULL);
-						switch(retval){
-							case -1:
-							perror("select failed\n");
-							done=1;
-							break;
-							case 0:
-							printf("CASE 0\n");
-							//break;
-							
-							default:
-							if(!connected){
-							printf("Not connected.\n");
-							break;
-						}
+retval=select(sock+1,&rfds,NULL,NULL,NULL);
+switch(retval){
+case -1:
+//perror("select failed\n");
+if(errno==EINTR){
+printf(red "EINTR occurred.\n");
+}
+done=1;
+break;
+case 0:
+printf("CASE 0\n");
+break;
+default:
+if(!connected){
+printf("Not connected.\n");
+break;
+}
 //if(FD_ISSET(sock,&rfds)){printf(yellow "DO HANDLE PG READ: retval: %d\n" rst,retval);handlepgread(conn);}
-							//break;
-							printf("default\n");
-							}
-							printf("end of while\n");
-				}
-			}
+printf("default\n");
+}
+printf("end of while\n");
+}
+}
+
 void initlisten(PGconn*conn){
 printf("Entering initlisten()\n");
 char*quotedchannel=PQescapeIdentifier(conn,listenchannel,strlen(listenchannel));
@@ -129,29 +153,55 @@ return;
 }
 }
 void handlepgread(PGconn*conn){
-	printf("entering handlepgread\n");
-	PGnotify*notify;
-	PGresult*res;
-	PQprintOpt opt;
-	if(!PQconsumeInput(conn)){
-	fprintf(stderr,"failed to consume input: %s\n",PQerrorMessage(conn));
-	return;
+printf("entering handlepgread(conn)\n");
+PGnotify*notify;
+PGresult*res;
+PQprintOpt opt;
+if(!PQconsumeInput(conn)){
+	printf("hhhh\n");
+fprintf(stderr,"failed to consume input: %s\n",PQerrorMessage(conn));
+done=1;
+exitclean(conn);
+return;
+}
+printf("before result\n");
+
+if(NULL==PQgetResult(conn)){
+printf("result NULL: %s\n",PQerrorMessage(conn));
+ConnStatusType status=PQstatus(conn);
+if(status==CONNECTION_BAD){printf("BAAAAAAAD\n");}else{printf("status unknown\n");}
+	
+	}else{printf("result is not null.\n");}
+	
+	/*
+switch(PQresultStatus(PQgetResult(conn))){
+	case PGRES_FATAL_ERROR:
+	printf("fatali erri: %s\n",PQresultErrorMessage(PQgetResult(conn)));
+	break;
+	default:
+	printf("fuck know aaa\n");
 	}
-		
-	while(res=PQgetResult(conn)){
-		if(PQresultStatus(res) !=PGRES_COMMAND_OK){
-			fprintf(stderr,"result err: %s\n",PQerrorMessage(conn));
-			PQclear(res);
-			return;
-			}
-			memset(&opt,'\0',sizeof(opt));
-			PQprint(stdout,res,&opt);
-			printf("got result\n");
-			fuck=1;
-			PQclear(res);
-		}
-		while(notify=PQnotifies(conn)){
-			fprintf(stderr,"notify of %s received from backend pid %d ,extra: %s\n",notify->relname,notify->be_pid,notify->extra);
-			PQfreemem(notify);
-			}
-	}
+	*/ 
+
+while((res=PQgetResult(conn)) !=NULL){
+	printf("in LOOP\n");
+if(PQresultStatus(res) !=PGRES_COMMAND_OK){printf("pgres command not ok\n");}else{printf("fuck knows\n");}
+if(PQresultStatus(res) !=PGRES_COMMAND_OK){
+fprintf(stderr,"result err: %s\n",PQerrorMessage(conn));
+PQclear(res);
+return;
+}
+memset(&opt,'\0',sizeof(opt));
+PQprint(stdout,res,&opt);
+printf("got result\n");
+fuck=1;
+PQclear(res);
+}
+
+printf("before notify\n");
+while(notify=PQnotifies(conn)){
+fprintf(stderr,yellow "notify of %s received from backend pid %d ,extra: %s\n" rst, notify->relname, notify->be_pid, notify->extra);
+PQfreemem(notify);
+}
+printf("END\n");
+}
